7b447ca60bb21d8abcf90a2d5254d7f3322c3e07
Madhusri182007 <madhusri182007@gmail.com>
2024-11-23 20:27:33 +0530

    Update and rename Sqrroot.c to 69Sqrroot.c

--- 69Sqrroot.c
+++ 69Sqrroot.c
@@ -1,7 +1,12 @@
 int mySqrt(int x) 
 {
-    int i,p=1;
-    for(i=1;i<=x/2;i++)
+  long  int i,p=1,C;
+    if(x==1)return 1;
+     else if(x==2|| x==3)
+        {
+            return 1;
+        }
+    for(i=1;i<=x;i++)
     {
         p=i*i;
         if(p==x)
