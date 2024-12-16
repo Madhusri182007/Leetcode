051547906b327b045cae93025f15b5afbfbb8bab
Madhusri182007 <madhusri182007@gmail.com>
2024-12-16 20:56:36 +0530

    Create 268_Missing_Number.c

---
+++ 268_Missing_Number.c
@@ -0,0 +1,14 @@
+int missingNumber(int* nums, int numsSize) {
+   int sum=0,s=0;
+   for(int i=0;i<numsSize;i++)
+   {
+    sum=nums[i]+sum;
+   }
+
+   for(int i=0;i<=numsSize;i++)
+   {
+    s=s+i;
+   }
+   int c=s-sum;
+   return c;
+}
